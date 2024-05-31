a6b78b26ce88391052cc015eb72fb0c3231ac6ad
Caio Balduino <caiobalduinodev@hotmail.com>
2024-05-31 17:17:15 -0300

    feat: background change script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const tabMenu = document.querySelectorAll(".js-tabmenu li");
 const tabContent = document.querySelectorAll(".js-tabcontent section");
 const tabImages = document.querySelectorAll(".js-tabimages section");
 const backLine = document.querySelector(".back-line");
+const body = document.body;
 
 if (tabMenu.length && tabContent.length && tabImages.length) {
   tabContent[0].classList.add("ativo");
@@ -25,11 +26,22 @@ if (tabMenu.length && tabContent.length && tabImages.length) {
     backLine.style.backgroundColor = colors[index];
   }
 
+  function changeBackground(index) {
+    const backgrounds = [
+      'url(./imgs/verao-bg.png)',
+      'url(./imgs/outono-bg.png)',
+      'url(./imgs/inverno-bg.png)',
+      'url(./imgs/primavera-bg.png)'
+    ];
+    body.style.background = `${backgrounds[index]} no-repeat center center / cover`;
+  }
+
   tabMenu.forEach((itemMenu, index) => {
     itemMenu.addEventListener("click", () => {
       activeTab(index);
       activeImages(index);
       changeBacklineColor(index);
+      changeBackground(index);
     });
   });
 }
